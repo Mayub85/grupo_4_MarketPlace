0f35c086b6503aa9b763e6b84858b0e2683ed10d
Mayub85 <marianoayub@hotmail.com>
2020-12-08 12:30:08 -0300

    Home con responsive

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -22,36 +22,26 @@ $(document).ready(()=>{
 
 
 function productsResize(){
-    var currentBreakpoint; // default's to blank so it's always analysed on first load
-    var didResize  = true; // default's to true so it's always analysed on first load
-    var raw_slider = $(".productsflexslider").html(); // grab the unaltered HTML and store it
+    var currentBreakpoint; 
+    var didResize  = true; 
+    var raw_slider = $(".productsflexslider").html(); 
   
-    // on window resize, set the didResize to true
     $(window).resize(function() {
       didResize = true;
     });
   
-    // every 1/4 second, check if the browser was resized
-    // we throttled this because some browsers fire the resize even continuously during resize
-    // that causes excessive processing, this helps limit that
     setInterval(function() {
       if(didResize) {
         didResize = false;
   
-        // inspect the CSS to see what breakpoint the new window width has fallen into
         var newBreakpoint = window.getComputedStyle(document.body, ':after').getPropertyValue('content');
   
-        /* tidy up after inconsistent browsers (some include quotation marks, they shouldn't) */
         newBreakpoint = newBreakpoint.replace(/"/g, "");
         newBreakpoint = newBreakpoint.replace(/'/g, "");
   
-        // if the new breakpoint is different to the old one, do some stuff
         if (currentBreakpoint != newBreakpoint) {
-  
-            // remove the old flexslider (which has attached event handlers and adjusted DOM nodes)
             $(".productsflexslider").remove();
     
-            // now re-insert clean mark-up so flexslider can run on it properly
             $(".productsSliderWrapper").append("<div class='productsflexslider'></div>");
             $(".productsflexslider").html(raw_slider);
     
@@ -62,7 +52,7 @@ function productsResize(){
                       animation: "slide",
                       animationLoop: false,
                       slideshow: false,
-                      itemWidth: 190,
+                      itemWidth: 95,
                       itemMargin: 0,
                       minItems: 1,
                       maxItems: 1,

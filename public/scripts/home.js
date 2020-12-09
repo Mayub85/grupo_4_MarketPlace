4f09344c99aca7b79067ca474c6759a0b03088fa
Mariano <SE14579@grupo.ypf.com>
2020-12-09 16:19:59 -0300

    Carrito de compras (falta ajustar el responsive)

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -21,107 +21,107 @@ $(document).ready(()=>{
 });
 
 
-function productsResize(){
-    var currentBreakpoint; 
-    var didResize  = true; 
-    var raw_slider = $(".productsflexslider").html(); 
+// function productsResize(){
+//     var currentBreakpoint; 
+//     var didResize  = true; 
+//     var raw_slider = $(".productsflexslider").html(); 
   
-    $(window).resize(function() {
-      didResize = true;
-    });
+//     $(window).resize(function() {
+//       didResize = true;
+//     });
   
-    setInterval(function() {
-      if(didResize) {
-        didResize = false;
+//     setInterval(function() {
+//       if(didResize) {
+//         didResize = false;
   
-        var newBreakpoint = window.getComputedStyle(document.body, ':after').getPropertyValue('content');
+//         var newBreakpoint = window.getComputedStyle(document.body, ':after').getPropertyValue('content');
   
-        newBreakpoint = newBreakpoint.replace(/"/g, "");
-        newBreakpoint = newBreakpoint.replace(/'/g, "");
+//         newBreakpoint = newBreakpoint.replace(/"/g, "");
+//         newBreakpoint = newBreakpoint.replace(/'/g, "");
   
-        if (currentBreakpoint != newBreakpoint) {
-            $(".productsflexslider").remove();
+//         if (currentBreakpoint != newBreakpoint) {
+//             $(".productsflexslider").remove();
     
-            $(".productsSliderWrapper").append("<div class='productsflexslider'></div>");
-            $(".productsflexslider").html(raw_slider);
+//             $(".productsSliderWrapper").append("<div class='productsflexslider'></div>");
+//             $(".productsflexslider").html(raw_slider);
     
-            switch (newBreakpoint) {
-                case 'breakpoint_1':
-                    currentBreakpoint = 'breakpoint_1';
-                    $(".productsflexslider").flexslider({
-                      animation: "slide",
-                      animationLoop: false,
-                      slideshow: false,
-                      itemWidth: 95,
-                      itemMargin: 0,
-                      minItems: 1,
-                      maxItems: 1,
-                      controlNav: false
-                    });
-                break;
-                case 'breakpoint_2':
-                    currentBreakpoint = 'breakpoint_2';
-                    $(".productsflexslider").flexslider({
-                        animation: "slide",
-                        animationLoop: false,
-                        slideshow: false,
-                        itemWidth: 190,
-                        itemMargin: 5,
-                        minItems: 1,
-                        maxItems: 2,
-                        controlNav: false
-                    });
-                break;
-                case 'breakpoint_3':
-                    currentBreakpoint = 'breakpoint_3';
-                    $(".productsflexslider").flexslider({
-                        animation: "slide",
-                        animationLoop: false,
-                        slideshow: false,
-                        itemWidth: 190,
-                        itemMargin: 5,
-                        minItems: 1,
-                        maxItems: 3,
-                        controlNav: false
-                    });
-                break;
-                case 'breakpoint_4':
-                    currentBreakpoint = 'breakpoint_4';
-                    $(".productsflexslider").flexslider({
-                        animation: "slide",
-                        animationLoop: false,
-                        slideshow: false,
-                        itemWidth: 190,
-                        itemMargin: 5,
-                        minItems: 3,
-                        maxItems: 4,
-                        controlNav: false
-                    });
-                break;
-                case 'breakpoint_5':
-                    currentBreakpoint = 'breakpoint_5';
-                    $(".productsflexslider").flexslider({
-                      animation: "slide",
-                      animationLoop: false,
-                      slideshow: false,
-                      itemWidth: 190,
-                      itemMargin: 5,
-                      minItems: 3,
-                      maxItems: 5,
-                      controlNav: false
-                    });
-                break;
+//             switch (newBreakpoint) {
+//                 case 'breakpoint_1':
+//                     currentBreakpoint = 'breakpoint_1';
+//                     $(".productsflexslider").flexslider({
+//                       animation: "slide",
+//                       animationLoop: false,
+//                       slideshow: false,
+//                       itemWidth: 95,
+//                       itemMargin: 0,
+//                       minItems: 1,
+//                       maxItems: 1,
+//                       controlNav: false
+//                     });
+//                 break;
+//                 case 'breakpoint_2':
+//                     currentBreakpoint = 'breakpoint_2';
+//                     $(".productsflexslider").flexslider({
+//                         animation: "slide",
+//                         animationLoop: false,
+//                         slideshow: false,
+//                         itemWidth: 190,
+//                         itemMargin: 5,
+//                         minItems: 1,
+//                         maxItems: 2,
+//                         controlNav: false
+//                     });
+//                 break;
+//                 case 'breakpoint_3':
+//                     currentBreakpoint = 'breakpoint_3';
+//                     $(".productsflexslider").flexslider({
+//                         animation: "slide",
+//                         animationLoop: false,
+//                         slideshow: false,
+//                         itemWidth: 190,
+//                         itemMargin: 5,
+//                         minItems: 1,
+//                         maxItems: 3,
+//                         controlNav: false
+//                     });
+//                 break;
+//                 case 'breakpoint_4':
+//                     currentBreakpoint = 'breakpoint_4';
+//                     $(".productsflexslider").flexslider({
+//                         animation: "slide",
+//                         animationLoop: false,
+//                         slideshow: false,
+//                         itemWidth: 190,
+//                         itemMargin: 5,
+//                         minItems: 3,
+//                         maxItems: 4,
+//                         controlNav: false
+//                     });
+//                 break;
+//                 case 'breakpoint_5':
+//                     currentBreakpoint = 'breakpoint_5';
+//                     $(".productsflexslider").flexslider({
+//                       animation: "slide",
+//                       animationLoop: false,
+//                       slideshow: false,
+//                       itemWidth: 190,
+//                       itemMargin: 5,
+//                       minItems: 3,
+//                       maxItems: 5,
+//                       controlNav: false
+//                     });
+//                 break;
             
-                default:
-                    break;
-            }
-        }
-      }
-    }, 250);
-}
+//                 default:
+//                     break;
+//             }
+//         }
+//       }
+//     }, 250);
+// }
 
 
-function addToFav(el){
-    alert("Se añade a favoritos: " + el.attributes["class"].value);
-    console.log(el);
-}
+// function addToFav(el){
+//     alert("Se añade a favoritos: " + el.attributes["class"].value);
+//     console.log(el);
+// }
---
+++ public/scripts/productCart.js
@@ -0,0 +1,51 @@
+$(document).ready(()=>{
+    setupNumberInputs();
+    $('.productsflexslider').flexslider({
+        animation: "slide",
+        controlNav: false,
+        slideshow: false,
+        itemWidth: 190,
+        itemMargin: 5,
+        maxItems: 5
+      });
+      productsResize();
+      
+    //FIX ancho del slider principal
+    setTimeout(()=>{fslider.resize();}, 200);
+});
+
+
+function setupNumberInputs (){
+    jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
+    jQuery('.quantity').each(function() {
+      var spinner = jQuery(this),
+        input = spinner.find('input[type="number"]'),
+        btnUp = spinner.find('.quantity-up'),
+        btnDown = spinner.find('.quantity-down'),
+        min = input.attr('min'),
+        max = input.attr('max');
+
+      btnUp.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue >= max) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue + 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+      btnDown.click(function() {
+        var oldValue = parseFloat(input.val());
+        if (oldValue <= min) {
+          var newVal = oldValue;
+        } else {
+          var newVal = oldValue - 1;
+        }
+        spinner.find("input").val(newVal);
+        spinner.find("input").trigger("change");
+      });
+
+    });
+}

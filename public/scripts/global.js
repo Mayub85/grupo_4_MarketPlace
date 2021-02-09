2e31bf7ede983558fbc8a7ffcb176f28a29865c7
Mayub85 <marianoayub@hotmail.com>
2021-02-09 16:27:29 -0300

    Fixes y se añade easter egg nuevo

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -131,3 +131,51 @@ function addToFav(el){
     alert("Se añade a favoritos: " + el.attributes["class"].value);
     console.log(el);
 }
+
+
+// a key map of allowed keys
+var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    65: 'a',
+    66: 'b'
+  };
+  
+  // the 'official' Konami Code sequence
+  var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+  
+  // a variable to remember the 'position' the user has reached so far.
+  var konamiCodePosition = 0;
+  
+  // add keydown event listener
+  document.addEventListener('keydown', function(e) {
+    // get the value of the key code from the key map
+    var key = allowedKeys[e.keyCode];
+    // get the value of the required key from the konami code
+    var requiredKey = konamiCode[konamiCodePosition];
+  
+    // compare the key with the required key
+    if (key == requiredKey) {
+  
+      // move to the next key in the konami code sequence
+      konamiCodePosition++;
+  
+      // if the last key is reached, activate cheats
+      if (konamiCodePosition == konamiCode.length) {
+        activateCheats();
+        konamiCodePosition = 0;
+      }
+    } else {
+      konamiCodePosition = 0;
+    }
+  });
+  
+  function activateCheats() {
+    var audio = new Audio('audio/eegg.mp3');
+    audio.play();
+    document.body.style.backgroundImage = "url('images/fede.jpg')";
+    document.body.style.backgroundRepeat = "repeat";
+    document.body.style.backgroundSize = "auto";
+  }
